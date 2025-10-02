563d8b68208d9c0aa28a847c886fdc92466f8233
Johann Paez <johannsebastian.paezg@gmail.com>
2025-10-02 15:26:01 -0500

    Initial commit

---
+++ src/test/java/com/spring/boot/project/ms/manga/store/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.boot.project.ms.manga.store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
