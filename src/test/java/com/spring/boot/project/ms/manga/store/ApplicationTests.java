b6f0452f3b122c3a83682df1dd53c11dd9dc0e37
Ricardo Paez <ricardoyepes7@hotmail.com>
2025-10-03 14:33:46 -0500

    Commit to check the pre-commit

--- src/test/java/com/spring/boot/project/ms/manga/store/ApplicationTests.java
+++ src/test/java/com/spring/boot/project/ms/manga/store/ApplicationTests.java
@@ -1,6 +1,7 @@
 package com.spring.boot.project.ms.manga.store;
 
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
@@ -9,5 +10,9 @@ class ApplicationTests {
 	@Test
 	void contextLoads() {
 	}
+    @Test
+    void prueba(){
+        assertTrue(false);
+    }
 
 }
