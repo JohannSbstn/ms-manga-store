b10c7d527f90181c3375a73b59d44d4a5b830845
Ricardo Paez <ricardoyepes7@hotmail.com>
2025-10-03 17:35:08 -0500

    checkstyle fail

--- src/test/java/com/spring/boot/project/ms/manga/store/ApplicationTests.java
+++ src/test/java/com/spring/boot/project/ms/manga/store/ApplicationTests.java
@@ -1,7 +1,6 @@
 package com.spring.boot.project.ms.manga.store;
 
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
@@ -10,9 +9,4 @@ class ApplicationTests {
 	@Test
 	void contextLoads() {
 	}
-    @Test
-    void prueba(){
-        assertTrue(false);
-    }
-
 }
