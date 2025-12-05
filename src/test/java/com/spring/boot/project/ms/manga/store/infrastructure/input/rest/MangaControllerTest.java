5a57df92f964d2f60124f684811f50a2d13c0e17
Marlon Velez Vasquez <marlon.velez@globant.com>
2025-12-05 10:11:28 -0500

    feat:MNST-10 added toked secret, valid second, remove anottation

--- src/test/java/com/spring/boot/project/ms/manga/store/infrastructure/input/rest/MangaControllerTest.java
+++ src/test/java/com/spring/boot/project/ms/manga/store/infrastructure/input/rest/MangaControllerTest.java
@@ -5,7 +5,6 @@ import com.spring.boot.project.ms.manga.store.application.service.MangaService;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
@@ -21,7 +20,6 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(MangaController.class)
-@AutoConfigureMockMvc(addFilters = false)
 class MangaControllerTest {
 
     @Autowired
